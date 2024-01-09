330ce31c621401d5899f0cd5542701c6094d0235
kridhvi <kridhvi07@gmail.com>
2024-01-09 22:30:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+class SmartMeterBilling {
+  constructor(peakHoursRate, offPeakHoursRate) {
+    this.peakHoursRate = peakHoursRate;
+    this.offPeakHoursRate = offPeakHoursRate;
+    this.accumulatedReadings = {};
+    this.totalBillAmount = {};
+  }
+
+  calculateBillAmount = (meterId, timestamp, meterReading) => {
+    const hour = new Date(timestamp).getHours();
+    const billingRate = hour >= 7 && hour < 24 ? this.peakHoursRate : this.offPeakHoursRate;
+
+    //Current Usuage = Previous meter reading - current meter reading
+    const hourlyUsage = this.accumulatedReadings[meterId] ? meterReading - this.accumulatedReadings[meterId] : meterReading;
+    const hourlyCost = billingRate * hourlyUsage;
+
+    // Accumulate the cost for this meter
+    if (this.totalBillAmount[meterId]) {
+      this.totalBillAmount[meterId] += hourlyCost;
+    } else {
+      this.totalBillAmount[meterId] = hourlyCost;
+    }
+
+    this.accumulatedReadings[meterId] = meterReading;
+  }
+
+  processUsageData = (electricityUsageData) => {
+    for (const [meterId, timestamp, meterReading] of electricityUsageData) {
+      this.calculateBillAmount(meterId, timestamp, meterReading);
+    }
+  }
+
+  getTotalBillAmount = () => {
+    console.log(this.totalBillAmount);
+    return this.totalBillAmount;
+  }
+};
+
+module.exports = SmartMeterBilling;
